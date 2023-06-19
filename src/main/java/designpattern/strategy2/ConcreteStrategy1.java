ad511bf25a2102eda33450e2b2085d7319b8e952
mabb <mbk1991@naver.com>
2023-06-19 11:45:32 +0900

    223.06.19 Composite 패턴

--- src/main/java/designpattern/strategy2/ConcreteStrategy1.java
+++ src/main/java/designpattern/strategy2/ConcreteStrategy1.java
@@ -2,7 +2,20 @@ package main.java.designpattern.strategy2;
 
 public class ConcreteStrategy1 implements Strategy{
     @Override
-    public void sortAlgorithm(){
+    public <T extends Comparable> void sortAlgorithm(T[] array){
         //selectionSort
+        System.out.println("선택정렬 실행");
+        int min;
+        for(int i=0; i<array.length; i++){
+            min = i;
+            for(int j=i+1; j<array.length; j++){
+                if(array[j].compareTo(array[min]) < 0){
+                    min = j;
+                }
+            }
+            T tmp = array[i];
+            array[i] = array[min];
+            array[min] = tmp;
+        }
     }
 }
--- src/main/java/designpattern/strategy2/ConcreteStrategy2.java
+++ src/main/java/designpattern/strategy2/ConcreteStrategy2.java
@@ -2,7 +2,17 @@ package main.java.designpattern.strategy2;
 
 public class ConcreteStrategy2 implements Strategy{
     @Override
-    public void sortAlgorithm() {
+    public <T extends Comparable> void sortAlgorithm(T[] array){
         //insertionSort
+        System.out.println("삽입정렬 실행");
+        for(int i = 1; i< array.length; i++){
+            T tmp = array[i];
+            int aux = i - 1;
+            while( (aux >= 0) && (array[aux].compareTo(tmp) > 0)){
+                array[aux + 1] = array[aux];
+                aux --;
+            }
+            array[aux + 1] = tmp;
+        }
     }
 }
